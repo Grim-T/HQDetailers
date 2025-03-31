3f2025c70b55602441a8eb66cd18193b12721fe9
Grim-T <138415043+Grim-T@users.noreply.github.com>
2025-04-01 00:30:01 +0100

    Create Home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to HQ Detailers</h1>
+      <p>We provide the best car detailing services.</p>
+    </div>
+  );
+};
+
+export default Home;
