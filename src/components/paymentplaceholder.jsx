570bc98b7d7daa17f61f8fdbcc38997b2cb0a376
Grim-T <138415043+Grim-T@users.noreply.github.com>
2025-04-01 00:41:40 +0100

    Create paymentplaceholder

---
+++ src/components/paymentplaceholder.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./PaymentPlaceholder.css";
+
+function PaymentPlaceholder() {
+  return (
+    <div className="payment-placeholder">
+      <h2>Payment System</h2>
+      <p>Stripe or Visa/MasterCard integration will be added here.</p>
+    </div>
+  );
+}
+
+export default PaymentPlaceholder;
