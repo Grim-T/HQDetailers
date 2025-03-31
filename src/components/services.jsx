1d430a360d17e4ce1c8c9b364a211ebc7491e865
Grim-T <138415043+Grim-T@users.noreply.github.com>
2025-04-01 00:34:24 +0100

    services page

---
+++ src/components/services.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Services.css";
+
+function Services() {
+  return (
+    <div className="services">
+      <h2>Our Services</h2>
+      <ul>
+        <li>Exterior Wash & Wax - £15</li>
+        <li>Interior Deep Clean - £30</li>
+        <li>Full Detailing Package - £50</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Services;
