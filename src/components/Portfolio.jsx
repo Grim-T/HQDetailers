74c630a208d882e7cb35832f3f62ac2d9568a80f
Grim-T <138415043+Grim-T@users.noreply.github.com>
2025-04-01 00:36:06 +0100

    Create Portfolio of cars

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Portfolio.css";
+
+function Portfolio() {
+  return (
+    <div className="portfolio">
+      <h2>Our Work</h2>
+      <img src="/car1.jpg" alt="Detailed Car 1" />
+      <img src="/car2.jpg" alt="Detailed Car 2" />
+      <img src="/car3.jpg" alt="Detailed Car 3" />
+    </div>
+  );
+}
+
+export default Portfolio;
