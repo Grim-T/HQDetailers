e3a70dc36457b9f4433a857a4b36ffdc04049ff6
Grim-T <138415043+Grim-T@users.noreply.github.com>
2025-04-01 01:14:44 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import logo from "../assets/hq-detailer-logo.png"; 
+import logo from "HQDetailers/src/assets/images/Image.jpeg"; // Adjust path if needed
 
-const Header = () => {
+function Header() {
   return (
     <header>
-      <img src={logo} alt="HQ Detailer Logo" className="logo" />
+      <img src={logo} alt="HQ Detailers Logo" style={{ width: "150px", height: "auto" }} />
     </header>
   );
-};
+}
 
 export default Header;
+
