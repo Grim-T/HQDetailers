d4592b18e7d5f24f6ed72c270a666adcfa9b59b5
Grim-T <138415043+Grim-T@users.noreply.github.com>
2025-04-10 04:28:13 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import Header from "./components/Header.jsx";
 import Navbar from "./components/Navbar.jsx";
 import Home from "./components/Home.jsx";
-import Services from "./components/Services.jsx";
+import Services from "./components/services.jsx";
 import Portfolio from "./components/Portfolio.jsx";
-import Contact from "./components/Contact.jsx";
+import Contact from "./components/contact.jsx";
 import Booking from "./components/Booking.jsx";
 import "./App.css";
 
