9e3e7d35c94ed18309640082280a8e7b52bda4c7
Grim-T <138415043+Grim-T@users.noreply.github.com>
2025-04-12 23:00:11 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 
-const Navbar = ({ setPage }) => {
+function Navbar({ setActivePage }) {
   return (
-    <nav>
-      <button onClick={() => setPage("home")}>Home</button>
-      <button onClick={() => setPage("services")}>Services</button>
-      <button onClick={() => setPage("portfolio")}>Portfolio</button>
-      <button onClick={() => setPage("contact")}>Contact</button>
-      <button onClick={() => setPage("booking")}>Book Now</button>
+    <nav className="navbar">
+      <ul>
+        <li onClick={() => setActivePage("Home")}>Home</li>
+        <li onClick={() => setActivePage("Services")}>Services</li>
+        <li onClick={() => setActivePage("Portfolio")}>Portfolio</li>
+        <li onClick={() => setActivePage("Contact")}>Contact</li>
+        <li onClick={() => setActivePage("Booking")}>Booking</li>
+      </ul>
     </nav>
   );
-};
+}
 
 export default Navbar;
+
