b9529f6c12f31093feffe48485cdd44e5b01c3f8
Grim-T <138415043+Grim-T@users.noreply.github.com>
2025-04-13 13:51:31 +0100

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,19 +15,19 @@ function Home() {
 
       <section className="card-section">
         <div className="card">
-          <img src={exteriorImg} alt="Exterior Detailing" />
+          //<img src={exteriorImg} alt="Exterior Detailing" />
           <h2>Exterior Perfection</h2>
           <p>From hand washes to ceramic coatings, we make your car shine like new.</p>
         </div>
 
         <div className="card">
-          <img src={interiorImg} alt="Interior Detailing" />
+          //<img src={interiorImg} alt="Interior Detailing" />
           <h2>Interior Detailing</h2>
           <p>Deep cleaning, shampoo, and leather care that revives your car’s interior.</p>
         </div>
 
         <div className="card">
-          <img src={mobileImg} alt="Mobile Service" />
+          //<img src={mobileImg} alt="Mobile Service" />
           <h2>Mobile Service</h2>
           <p>We bring our professional service straight to your driveway, saving you time.</p>
         </div>
